70b65d385366a210d5216b563486932a4c85e503
Chaitanya Nayak <CJ@Chaitanyas-MacBook-Pro.local>
2019-07-05 01:02:41 -0500

    Recipe Controller

--- src/main/java/com/eatwell/repository/RecipeRepository.java
+++ src/main/java/com/eatwell/repository/RecipeRepository.java
@@ -1,4 +1,12 @@
 package com.eatwell.repository;
 
-public class RecipeRepository {
+import com.eatwell.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Creates a Spring JPA Repository for the recipes
+ * Provides default implementation for basic methods - findOne, findAll, delete, saveAndFlush
+ */
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
 }
